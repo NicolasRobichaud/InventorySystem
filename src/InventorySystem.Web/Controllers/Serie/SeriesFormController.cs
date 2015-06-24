9d9e47640b2befaaa0df615c7c0247b6d39b9b0e
Nicolas Robichaud <nicolasrobichaudb@gmail.com>
2015-06-24 13:49:57 -0400

    Series form controller

---
+++ src/InventorySystem.Web/Controllers/Serie/SeriesFormController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using InventorySystem.Data.Manager;
+using InventorySystem.Data.Repositories;
+using InventorySystem.Data.Entity;
+using InventorySystem.Web.ViewModel.Series;
+using InventorySystem.Data.Helper;
+
+namespace InventorySystem.Web.Controllers.Serie
+{
+    [Route("series")]
+    public class SeriesFormController : Controller
+    {
+        private const string ViewName = "EditForm";
+        private readonly BaseRepository _baseRepository;
+
+        public SeriesFormController(BaseRepository baseRepository)
+        {
+            _baseRepository = baseRepository;
+        }
+
+        [HttpGet]
+        [Route("edit")]
+        public IActionResult Edit()
+        {
+            return View(ViewName);
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public async Task<IActionResult> EditAsync(string id)
+        {
+            var brand = await _baseRepository.LoadAsync<SeriesEntity>(ParserHelper.ToGuid(id));
+
+            return View(ViewName, new SeriesFormViewModel
+            {
+                Id = brand?.Id.ToString(),
+                Name = brand?.Name
+            });
+        }
+
+        [HttpPost]
+        [Route("edit/{id?}")]
+        public async Task<IActionResult> SubmitAsync(SeriesFormViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _baseRepository.CreateOrUpdateAsync(ParserHelper.ToGuid(viewModel.Id),
+                    c =>
+                    {
+                        c.Name = viewModel.Name;
+                    },
+                    () =>
+                    {
+                        return new SeriesEntity
+                        {
+                            Name = viewModel.Name
+                        };
+                    });
+            }
+
+            return View(ViewName, viewModel);
+        }
+    }
+}
